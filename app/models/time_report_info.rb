3cd5c53d4b42cf63a6d8b68f119fd3d7efd885c9
Viktorija Bezsazna <viktorija.bezsazna@gmail.com>
2017-10-19 23:44:58 -0400

    Add validation to DB models

--- app/models/time_report_info.rb
+++ app/models/time_report_info.rb
@@ -1,4 +1,12 @@
 class TimeReportInfo < ApplicationRecord
+  validates :report_date, presence: true
+  validates :pay_period_start, presence: true
+  validates :pay_period_end, presence: true
+  validates :hours_worked, presence: true
+  validates :employee_id, presence: true
+  validates :job_group, presence: true
+  validates :report_id, presence: true
+
   def self.get_grouped_reports
     select("employee_id, pay_period_start,
                  pay_period_end, job_group,
